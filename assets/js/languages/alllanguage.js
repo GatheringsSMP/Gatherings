8416e1fdf34f7e78d35a0100888e70bee73c6a3e
TelonaHyugaRinne <GatheringsSMP@yandex.ru>
2025-10-03 14:14:41 +0300

    new design

--- assets/js/languages/alllanguage.js
+++ assets/js/languages/alllanguage.js
@@ -1,26 +1,21 @@
 const langArr = {
     "tswitch" : {
-        "ru": "Смена темы",
-        "en": "Theme switch",
+        "ru": "Тема",
+        "en": "Theme",
     },
 
     "homelink" : {
-        "ru": '<a href="index.html#ru" style="width: 13vw;"><h3 class="linkh3">&#10224; Главная</h3></a>',
-        "en": '<a href="index.html#en" style="width: 13vw;"><h3 class="linkh3">&#10224; Home</h3></a>',
-    },
-
-    "inflink" : {
-        "ru": '<a href="info.html#ru" style="width: 13vw;"><h3 class="linkh3">&#9885; Информация</h3></a>',
-        "en": '<a href="info.html#en" style="width: 13vw;"><h3 class="linkh3">&#9885; Info</h3></a>',
+        "ru": '<a href="index.html#ru" style="width: 13vw;"><h3 class="linkh3">Главная</h3></a>',
+        "en": '<a href="index.html#en" style="width: 13vw;"><h3 class="linkh3">Home</h3></a>',
     },
     
     "partlink" : {
-        "ru": '<a href="partners.html#ru" style="width: 13vw;"><h3 class="linkh3">&#9884; Партнёры</h3></a>',
-        "en": '<a href="partners.html#en" style="width: 13vw;"><h3 class="linkh3">&#9884; Partners</h3></a>',
+        "ru": '<a href="partners.html#ru" style="width: 13vw;"><h3 class="linkh3">Партнёры</h3></a>',
+        "en": '<a href="partners.html#en" style="width: 13vw;"><h3 class="linkh3">Partners</h3></a>',
     },
 
     "playerslink" : {
-        "ru": '<a href="players.html#ru" style="width: 13vw;"><h3 class="linkh3">&#12304; Игроки &#12305;</h3></a>',
-        "en": '<a href="players.html#en" style="width: 13vw;"><h3 class="linkh3">&#12304; Players &#12305;</h3></a>',
+        "ru": '<a href="players.html#ru" style="width: 13vw;"><h3 class="linkh3">Игроки</h3></a>',
+        "en": '<a href="players.html#en" style="width: 13vw;"><h3 class="linkh3">Players</h3></a>',
     },
 }
--- assets/js/languages/indlanguage.js
+++ assets/js/languages/indlanguage.js
@@ -3,4 +3,24 @@ const langInd = {
         "ru": '<h1 class="TTBT">Привет!</h1><br><br><h2 class="TTBT"> - Ты попавл на официальный сайт Gatherings SMP</h2><br><br><h2 class="TTBT"> - Тут ты можешь узнать инфорацию об игроках нашего сервера, узнать наше ядро или версию и многое другое!</h2>',
         "en": '<h1 class="TTBT">Hello!</h1><br><br><h2 class="TTBT"> - You have reached the official Gatherings SMP website</h2><br><br><h2 class="TTBT"> - Here you can find out information about the players on our server, find out our core or version, and much more!</h2>',
     },
+    
+    "minver" : {
+        "ru": "Minecraft<br> -В основном мы играем на версиях:<br><br> 1.20.1 - из-за прекращения поддержки порта мода origin для forge или neoforge<br><br> 1.21.5 - так как она одна из самых новых и удовлетворяет нашим требованиям",
+        "en": "Minecraft<br> -We mainly play on versions:<br><br> 1.20.1 - due to the end of support for the origin mod port for forge or neoforge<br><br> 1.21.5 - since it is one of the newest and meets our requirements",
+    },
+
+    "skin" : {
+        "ru": 'Система скинов<br><br> Мы используем официальные скины, а так же скины <a href="https://ely.by" class="text">Ely.by</a>',
+        "en": 'Skins system<br><br> We use official skins, as well as skins <a href="https://ely.by" class="text">Ely.by</a>',
+    },
+
+    "spigot" : {
+        "ru": 'Ядро<br><br> Мы используем ядро <a href="https://www.spigotmc.org/" class="text">Spigot</a><br> (да, сис-админша очень старая)<br><br> Это очень стабильное ядро, с большой базой плагинов',
+        "en": 'Core<br><br> We use the core <a href="https://www.spigotmc.org/" class="text">Spigot</a><br> (yes, system admin is very old)<br><br> This is very stable core and this core have very more plugins',
+    },
+
+    "mods" : {
+        "ru": 'Моды<br><br> Мы используем загрузчик <a href="https://neoforged.net" class="text">NeoForge</a><br> А так же сборку модов для межсезонья собирает сис-админша<br><br>Этот загрузчик очень стабильный и удобный, а так же моды на нём такие же качесьвенные, как и на forge',
+        "en": 'Mods<br><br> We use this modloader: <a href="https://neoforged.net" class="text">NeoForge</a><br> And modpacks for off-seasons made be system admin<br><br> This loader is very stable, comfortable, and mods for this loader the same as on forge loader',
+    },
 }
